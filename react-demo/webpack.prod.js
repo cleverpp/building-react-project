4c10a787e04cd69eea26d04f920810c9d37346d0
cpzhang <cpzhang>
2018-05-09 10:03:15 +0800

    webpack4 production mode 会启用 uglifyjs 压缩插件

--- react-demo/webpack.prod.js
+++ react-demo/webpack.prod.js
@@ -4,15 +4,15 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.config.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+/*const UglifyJsPlugin = require('uglifyjs-webpack-plugin');*/
 
 module.exports = merge(base,{
     mode:'production',
     /*devtool: 'source-map',*/
     plugins:[
-        new UglifyJsPlugin({
+        /*new UglifyJsPlugin({
             sourceMap:true
-        }),
+        }),*/
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
